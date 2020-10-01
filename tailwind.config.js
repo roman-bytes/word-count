0893c6ae87fbf8d86dd97f37db06b9fea2b2c288
Jacob Roman <jroman@mindgruve.com>
2020-09-30 22:07:22 -0400

    updated readme and purge tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,6 @@ module.exports = {
         // removeDeprecatedGapUtilities: true,
         // purgeLayersByDefault: true,
     },
-    purge: [
-        './src/components/**/*.{ts,tsx,js,jsx}',
-        './src/pages/**/*.{ts,tsx,js,jsx}',
-    ],
     theme: {
         extend: {
             spacing: {
